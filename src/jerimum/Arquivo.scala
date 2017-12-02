d888c691d37fff38c291cbcc093ade18d1069345
Thiago Demas <thiago.demas7@gmail.com>
2017-12-02 20:33:52 -0300

    Modificação da Classe Arquivo

--- src/jerimum/Arquivo.scala
+++ src/jerimum/Arquivo.scala
@@ -1,10 +1,10 @@
 package jerimum
 
-import br.edu.ifrn.potigol.Potigolutil._
+
 import scala.io.Source
 
 object Arquivo {
-  def leia(caminho: Texto): Lista[Texto] = {
-    Lista(Source.fromFile(caminho).getLines().toList)
+  def leia(caminho: String) {
+    List(Source.fromFile(caminho).getLines().toList)
   }
 }
