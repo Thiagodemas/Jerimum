6683c3d7276ec0a82402302b94ecf77e6827af5f
Thiago Demas <thiago.demas7@gmail.com>
2017-12-10 12:58:48 -0300

    modificações CatchTheStars

--- src/exemplo/CatchTheStars.scala
+++ src/exemplo/CatchTheStars.scala
@@ -4,6 +4,7 @@ import scala.util.Random
 import java.awt.Color
 import java.util.Random;
 import scala.util.Random;
+import com.sun.org.apache.bcel.internal.generic.Select
 
 
 object CatchTheStars extends App {
@@ -41,7 +42,7 @@ object CatchTheStars extends App {
     }
 
     def catch_the_stars(stars: List[Star]) = {
-      val caught = stars.select {
+      val caught = Select.stars {
         star => Game.distance(x, y, star.x, star.y) >= 35
       }
       val n = stars.size - caught.size
@@ -58,85 +59,87 @@ object CatchTheStars extends App {
     val images = Image.fatie("Estrela.png", 25, 25)
     val i = r.nextInt(images.size)
 
-/*    
+    
     
     def draw() = {
       val image = images((Clock.milisegundos / 100 + i) % images.size)
       image.centralized_draw(x, y, 1)
+      
     }
   }
   
-  val imagem_fundo = Imagem("Space.png")
-  val nave = Jogador(jogo.largura / 2, jogo.altura / 2)
-  var tempo = 0.0
-  var estrelas = List(0, Estrela())
-  var estado = "INICIO"
-  val fonte = Fonte(16)
+  val background = Image("Space.png")
+  val spacecraft = Player(game.width / 2, game.height / 2)
+  var time = 0.0
+  var stars = List(0, Star())
+  var state = "START"
+  val font = Fonts(16)
 
-  def atualize() = {
-    estado match {
-      case "INICIO"  => atualize_inicio
-      case "JOGANDO" => atualize_jogando
-      case _         => atualize_fim
+  def update() = {
+    state match {
+      case "START"  => update_start
+      case "PLAYING" => update_playing
+      case _         => update_end
     }
   }
 
-  def desenhe() = {
-    imagem_fundo.desenhe(0, 0, 0)
-    estado match {
-      case "INICIO"  => desenhe_inicio
-      case "JOGANDO" => desenhe_jogando
-      case _         => desenhe_fim
+  def draw() = {
+    background.draw(0, 0, 0)
+    state match {
+      case "START"  => update_start
+      case "PLAYING" => update_playing
+      case _         => update_end
     }
   }
 
   // Estado Inicio
-  def atualize_inicio() = {
-    if (Teclado.TECLA_I) estado = "JOGANDO"
+  def update_start() = {
+    if (Keyboard.KEY_I) state = "PLAYING"
   }
 
-  def desenhe_inicio() = {
+  def draw_start() = {
     val msg = "PRESSIONE [ I ] PARA COMEÇAR"
-    fonte.desenhe_centralizado(msg, jogo.largura / 2, jogo.altura / 2, 3, Cor.AMARELO)
+    font.draw_centered(msg, game.width / 2, game.height / 2, 3, ColorF.YELLOW)
   }
 
   // Estado Jogando
-  def atualize_jogando() = {
+  def update_playing() = {
+    val r = scala.util.Random
     // eventos
-    if (Teclado.TECLA_PARA_DIREITA) nave.girar_direita
-    if (Teclado.TECLA_PARA_ESQUERDA) nave.girar_esquerda
-    if (Teclado.TECLA_PARA_CIMA) nave.acelerar
+    if (Keyboard.RIGHT) spacecraft.rotate_right
+    if (Keyboard.LEFT) spacecraft.rotate_left
+    if (Keyboard.UP) spacecraft.accelerate
     // inserir novas estrelas estrelas se necessario
-    if (Random(100) < 4 && estrelas.size < 25) {
-      estrelas = Estrela() :: estrelas
+    if (r.nextInt(100) < 4 && stars.size < 25) {
+      stars = Star() :: stars
     }
 
-    estrelas = nave.catar_estrelas(estrelas) // catar estrelas
-    nave.mover // atualizar a posicao do jogador
-    tempo = tempo + 1.0 / 60.0 // incrementar o tempo
-    if (tempo.inteiro >= 30) {
-      estado = "FIM" // terminar o jogo depois de 30 segundos
+    stars = spacecraft.catch_the_stars(stars) // catar estrelas
+    spacecraft.move // atualizar a posicao do jogador
+    time = time + 1.0 / 60.0 // incrementar o tempo
+    if (time.intValue() >= 30) {
+      state = "END" // terminar o jogo depois de 30 segundos
     }
   }
 
-  def desenhe_jogando() = {
-    nave.desenhe
-    for (estrela <- estrelas) {
-      estrela.desenhe
+  def draw_player() = {
+    spacecraft.draw
+    for (star <- stars) {
+      star.draw
     }
-    fonte.desenhe(s"Placar: ${nave.placar}", 10, 20, 3, Cor.AMARELO)
-    fonte.desenhe(s"Tempo: ${tempo.toInt}s", 10, 40, 3, Cor.AMARELO)
+    font.draw(s"Placar: ${spacecraft.score}", 10, 20, 3, ColorF.YELLOW)
+    font.draw(s"Time: ${time.toInt}s", 10, 40, 3, ColorF.YELLOW)
   }
 
   // Estado: fim do jogo
-  def desenhe_fim() = {
-    val msg = s"FIM DE JOGO, VOCE FEZ ${nave.placar} PONTOS"
-    fonte.desenhe_centralizado(msg, jogo.largura / 2, jogo.altura / 2, 3, Cor.AMARELO)
+  def draw_end() = {
+    val msg = s"GAME OVER, VOCE FEZ ${spacecraft.score} PONTOS"
+    font.draw_centered(msg, game.width / 2, game.height / 2, 3, ColorF.YELLOW)
   }
 
-  def atualize_fim() = {}
+  def update_end() = {}
 
-  jogo.iniciar("Cata Estrelas", 640, 480, atualize, desenhe)
-*/
+  game.start("Cata Estrelas", 640, 480, update, draw)
+
   
   }
